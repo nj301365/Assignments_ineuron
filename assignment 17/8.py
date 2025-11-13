9a8f5d47b93c8908364d1dd483815aaaf0106651
nj301365 <100575737+nj301365@users.noreply.github.com>
2025-11-13 21:53:21 +0530

    Add example of set deletion and error handling
    Demonstrate deletion of a set and its access error.

---
+++ assignment 17/8.py
@@ -0,0 +1,7 @@
+thisset = {"Python", "Django", "JavaScript", "SQL"}
+
+# Delete the set
+del thisset
+
+# Accessing it now will give an error
+# print(thisset)  # Uncommenting this will raise NameError
