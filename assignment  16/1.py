2aa5f1c8bbb1b64e0d14cfd53e11f1314e856f84
nj301365 <100575737+nj301365@users.noreply.github.com>
2025-11-13 21:23:25 +0530

    Add tuple of programming languages and print it

--- assignment  16/1.py
+++ assignment  16/1.py
@@ -1,1 +1,5 @@
-print("jd")
+# Creating a tuple to store multiple items
+programming_languages = ("Java", "Python", "SQL", "C")
+
+# Print the tuple
+print("Stored programming languages:", programming_languages)
