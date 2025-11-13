eaa968e48249689c8b43aa6a6c43332e1ba52318
nj301365 <100575737+nj301365@users.noreply.github.com>
2025-11-13 21:31:09 +0530

    Create 4.py

---
+++ assignment  16/4.py
@@ -0,0 +1,11 @@
+# Define two tuples
+t1 = (43, 433)
+t2 = (23, 42)
+
+# Swap tuples using Python's tuple unpacking
+t1, t2 = t2, t1
+
+# Print the result
+print("After swapping:")
+print("t1 =", t1)
+print("t2 =", t2)
