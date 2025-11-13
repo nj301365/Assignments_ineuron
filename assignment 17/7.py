8015480d6f9e5418d6ce4cb5652074660ac75e76
nj301365 <100575737+nj301365@users.noreply.github.com>
2025-11-13 21:52:56 +0530

    Create 7.py

---
+++ assignment 17/7.py
@@ -0,0 +1,6 @@
+thisset = {"Python", "Django", "JavaScript", "SQL"}
+
+# Remove an arbitrary item
+removed_item = thisset.pop()
+print("Removed item:", removed_item)
+print("Set after removal:", thisset)
