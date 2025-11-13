af0447442386f65247386388362cc102c16cc372
nj301365 <100575737+nj301365@users.noreply.github.com>
2025-11-13 21:44:54 +0530

    Modify first element of list in tuple

---
+++ assignment  16/10.py
@@ -0,0 +1,8 @@
+# Given tuple
+tuple1 = (11, [22, 33], 44, 55)
+
+# Change the first element of the list inside the tuple
+tuple1[1][0] = 222
+
+# Print the modified tuple
+print("Modified tuple:", tuple1)
