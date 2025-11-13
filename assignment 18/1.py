b528bcd0bef86ac85beb6f2d323467be4f163d64
nj301365 <100575737+nj301365@users.noreply.github.com>
2025-11-13 21:58:17 +0530

    Add personal information dictionary in 1.py

---
+++ assignment 18/1.py
@@ -0,0 +1,9 @@
+# Dictionary storing personal information
+my_info = {
+    "name": "nikhil",
+    "age": 24,
+    "gender": "Male",
+    "profession": "student"
+}
+
+print("My information:", my_info)
