993e17a2b57f92e9ebd7f0e539795bd3796fa403
nj301365 <100575737+nj301365@users.noreply.github.com>
2025-11-13 22:04:46 +0530

    Create 7.py

---
+++ assignment 18/7.py
@@ -0,0 +1,6 @@
+dict1 = {"name": "Alice"}
+dict2 = {"name": "Bob"}
+dict3 = {"name": "Charlie"}
+
+combined_dict = {"first": dict1, "second": dict2, "third": dict3}
+print("Combined dictionary:", combined_dict)
