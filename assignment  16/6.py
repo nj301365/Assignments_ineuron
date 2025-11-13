aeb51d8a0c33683488cf34904562893bf624bcc6
nj301365 <100575737+nj301365@users.noreply.github.com>
2025-11-13 21:39:49 +0530

    Add tuple unpacking example in 6.py

---
+++ assignment  16/6.py
@@ -0,0 +1,11 @@
+# Define the tuple
+tuple1 = (100, 200, 300, 400)
+
+# Unpack the tuple into four variables
+a, b, c, d = tuple1
+
+# Print the variables
+print("a =", a)
+print("b =", b)
+print("c =", c)
+print("d =", d)
