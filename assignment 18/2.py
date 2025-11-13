35730018af42b4d3fe98bd1c88e4c2db68f286f1
nj301365 <100575737+nj301365@users.noreply.github.com>
2025-11-13 21:59:20 +0530

    Add personal information dictionary in 2.py

---
+++ assignment 18/2.py
@@ -0,0 +1,9 @@
+my_info = {
+    "name": "nikhil",
+    "age": 24,
+    "gender": "Male",
+    "profession": "nikhil"
+}
+# Access by key
+print("Name:", my_info["name"])
+print("Age:", my_info["age"])
